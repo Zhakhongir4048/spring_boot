5a75aad977e1a154ac63343e7924dce3d4152eab
Эсонов Жахонгир <Zhakhongir.Esonov@softline.com>
2022-05-21 18:51:18 +0300

    Spring Boot, Spring Rest

--- src/main/java/com/example/spring_boot/entity/Employee.java
+++ src/main/java/com/example/spring_boot/entity/Employee.java
@@ -33,11 +33,4 @@ public class Employee {
     @Column(name = "salary")
     private int salary;
 
-    public Employee(String name, String surname, String department, int salary) {
-        this.name = name;
-        this.surname = surname;
-        this.department = department;
-        this.salary = salary;
-    }
-
 }
--- src/main/java/com/example/spring_boot/service/EmployeeService.java
+++ src/main/java/com/example/spring_boot/service/EmployeeService.java
@@ -7,12 +7,12 @@ import java.util.List;
 
 public interface EmployeeService {
 
-    public List<Employee> getAllEmployees();
+    List<Employee> getAllEmployees();
 
-    public void saveEmployee(Employee employee);
+    void saveEmployee(Employee employee);
 
-    public Employee getEmployee(int id);
+    Employee getEmployee(int id);
 
-    public void deleteEmployee(int id);
+    void deleteEmployee(int id);
 
 }
